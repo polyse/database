7c2fc2c155c813fb16293ec0a84fdc7479f4086a
ivan <tarasovia1998@gmail.com>
2020-04-29 13:23:13 +0300

    Create filers

--- cmd/filter/filter.go
+++ cmd/filter/filter.go
@@ -1,17 +1,54 @@
 package filter
 
-import "strings"
+import (
+	"strings"
+
+	"github.com/kljensen/snowball/english"
+	"github.com/zoomio/stopwords"
+)
 
 // Filters is struct for input required filters
 type Filters struct {
 	Stemming  bool
-	ToLower bool
+	ToLower   bool
 	StopWords bool
 }
 
 // TextHandle is returning a slice of tokens after filtering
 func TextHandle(text string, f Filters) []string {
-	var tokens, output []string
+	var output []string
 
+	if f.ToLower {
+		text = strings.ToLower(text)
+	}
+
+	output = strings.Fields(text)
+
+	if f.StopWords {
+		output = output.stopWordsDelete()
+	}
+
+	if f.Stemming {
+		output = output.stemming()
+	}
+
+	return output
+}
+
+func (tokens []string) stopWordsDelete() []string {
+	var output []string
+	for _, token := range tokens {
+		if !stopwords.IsStopWord(token) {
+			output = append(output, token)
+		}
+	}
+	return output
+}
+
+func (tokens []string) stemming() []string {
+	var output []string
+	for _, token := range tokens {
+		output = append(output, english.Stem(token, false))
+	}
 	return output
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/polyse/database
 
 go 1.14
+
+require (
+	github.com/kljensen/snowball v0.6.0
+	github.com/zoomio/stopwords v0.5.0
+)
